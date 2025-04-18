1d483732ba3ab0b6c6a1f2bcc506996a1d37a4cc
Carlos Rebolledo Bernal <carlosrebolledobernal200@gmail.com>
2025-04-18 03:32:19 -0600

    sas

--- src/main/java/com/gym/controller/InstalacionesController.java
+++ src/main/java/com/gym/controller/InstalacionesController.java
@@ -1,13 +1,18 @@
 package com.gym.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
-public class InstalacionesController {
-    
-    @GetMapping("/instalaciones")
-    public String mostrarInstalaciones() {
-        return "instalaciones";
-    }
-}
+ package com.gym.controller;
+ 
+ import org.springframework.stereotype.Controller;
+ import org.springframework.web.bind.annotation.GetMapping;
+ 
+ @Controller
+ public class InstalacionesController {
+     
+     @GetMapping("/instalaciones")
+     public String mostrarInstalaciones() {
+         return "instalaciones";
+     }
+ }
+
+
